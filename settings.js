d104cf350320a69e8250cf4bceed6acdcd4dd051
Fahrul6868 <117023435+Fahrul6868@users.noreply.github.com>
2023-08-08 02:47:27 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "6692303964:AAEKVPffjtE5twsFuAntqOJOnA9mKqpgnow" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "Zero-Mdོ" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ Fahrul_cོ ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6285712408309" //your telegram number
+global.OWNER = ["https://t.me/Fahrul_mt", "https://t.me/Fahrul_mt"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
